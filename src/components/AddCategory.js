df6ef4ae76f7cdc5cebaffef3d31d5fc6a5b6f72
jose luis reyes <joseluisreyesalvarez97@gmail.com>
2021-09-02 14:41:11 -0500

    Primer commit

---
+++ src/components/AddCategory.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types';
+
+
+export const AddCategory = ({setCategories}) => {
+    const [inputValue, SetInputValue] = useState('');
+    const handleInputChange = (e) => {
+        SetInputValue(e.target.value)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (inputValue.trim().length > 2) {
+            setCategories(arr =>[inputValue,...arr])
+            SetInputValue('')
+
+        }
+        
+    }
+
+
+    return (
+
+        <form onSubmit={handleSubmit}>
+            <input
+                type='text'
+                value={inputValue}
+                onChange={handleInputChange}
+            />
+        </form>
+
+    )
+}
+AddCategory.propTypes ={
+    setCategories: PropTypes.func.isRequired
+}
---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react"
+import { getGifs } from "../helpers/geetGifs";
+
+export const useFetchGifs = (category)=>{
+    const [state, setState] = useState({
+        data:[],
+        loading:true
+    });
+
+    useEffect(()=>{
+
+        getGifs(category)
+        .then(imgs =>{
+
+            setTimeout(() => {
+                setState({
+                    data:imgs,
+                    loading:false
+                });
+            }, 1000);
+            
+        })
+
+    },[category])
+
+
+    return state;
+}
